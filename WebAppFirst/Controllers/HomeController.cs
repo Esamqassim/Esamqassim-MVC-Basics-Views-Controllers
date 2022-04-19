1e5412f78d7f90ce11f327076cf18af4fc44a309
Esamqassim <98977790+Esamqassim@users.noreply.github.com>
2022-04-19 04:19:22 +0200

    Add project files.

---
+++ WebAppFirst/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult About()//Aboute file view
+        {
+            return View();
+        }
+        //added 
+        public IActionResult Contact()//Contact file that u do not have
+        {
+            return View();
+        }
+        public IActionResult Projects()//Projects file that u do not have
+        {
+            return View();
+        }
+    }
+}
